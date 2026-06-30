f66894a7b2641f9f4591402e71675bcf8b56abdf
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-06-30 13:35:00 -0300

    feat: add fix to area arch

--- src/modules/tactical-board/components/FootballPitch.tsx
+++ src/modules/tactical-board/components/FootballPitch.tsx
@@ -17,10 +17,27 @@ function FootballPitchComponent() {
   return (
     <Group>
       <Rect width={BOARD_WIDTH} height={BOARD_HEIGHT} fill="#157347" />
-      <Rect x={18} y={18} width={BOARD_WIDTH - 36} height={BOARD_HEIGHT - 36} stroke={LINE_COLOR} strokeWidth={LINE_WIDTH} />
+      <Rect
+        x={18}
+        y={18}
+        width={BOARD_WIDTH - 36}
+        height={BOARD_HEIGHT - 36}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+      />
 
-      <Line points={[BOARD_WIDTH / 2, 18, BOARD_WIDTH / 2, BOARD_HEIGHT - 18]} stroke={LINE_COLOR} strokeWidth={LINE_WIDTH} />
-      <Circle x={BOARD_WIDTH / 2} y={centerY} radius={95} stroke={LINE_COLOR} strokeWidth={LINE_WIDTH} />
+      <Line
+        points={[BOARD_WIDTH / 2, 18, BOARD_WIDTH / 2, BOARD_HEIGHT - 18]}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+      />
+      <Circle
+        x={BOARD_WIDTH / 2}
+        y={centerY}
+        radius={95}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+      />
       <Circle x={BOARD_WIDTH / 2} y={centerY} radius={5} fill={LINE_COLOR} />
 
       <Rect
@@ -41,10 +58,10 @@ function FootballPitchComponent() {
       />
       <Circle x={142} y={centerY} radius={5} fill={LINE_COLOR} />
       <Arc
-        x={142}
+        x={128}
         y={centerY}
-        innerRadius={92}
-        outerRadius={92}
+        innerRadius={120}
+        outerRadius={120}
         angle={96}
         rotation={-48}
         stroke={LINE_COLOR}
@@ -69,18 +86,32 @@ function FootballPitchComponent() {
       />
       <Circle x={BOARD_WIDTH - 142} y={centerY} radius={5} fill={LINE_COLOR} />
       <Arc
-        x={BOARD_WIDTH - 142}
+        x={BOARD_WIDTH - 128}
         y={centerY}
-        innerRadius={92}
-        outerRadius={92}
+        innerRadius={120}
+        outerRadius={120}
         angle={96}
         rotation={132}
         stroke={LINE_COLOR}
         strokeWidth={LINE_WIDTH}
       />
 
-      <Rect x={0} y={centerY - 64} width={18} height={128} stroke={LINE_COLOR} strokeWidth={LINE_WIDTH} />
-      <Rect x={BOARD_WIDTH - 18} y={centerY - 64} width={18} height={128} stroke={LINE_COLOR} strokeWidth={LINE_WIDTH} />
+      <Rect
+        x={0}
+        y={centerY - 64}
+        width={18}
+        height={128}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+      />
+      <Rect
+        x={BOARD_WIDTH - 18}
+        y={centerY - 64}
+        width={18}
+        height={128}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+      />
     </Group>
   );
 }
